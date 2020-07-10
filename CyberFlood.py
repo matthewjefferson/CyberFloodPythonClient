eaaa85b3c8ac99c6158231afbf2714763d946a03
Jefferson, Matthew (Matt) <Matthew.Jefferson@spirent.com>
2020-07-10 16:45:36 -0700

    This will be the release code, once I've tested it thoroughly.

--- CyberFlood.py
+++ CyberFlood.py
@@ -1,21 +1,101 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+# This may help with Python 2/3 compatibility.
+
+# The next line is intentionally blank.
+
+__author__ = "Matthew Jefferson"
+__version__ = "1.0.0"
+
+# The previous line is intentionally blank.
+
+"""
+     Spirent CyberFlood Python Client
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    This module provides a Python front-end for the CyberFlood ReST API.
+    There are two, non-exclusive ways to execute commands: HTTP verbs and Perform Commands.
+    The supported HTTP verbs are "post", "put", "get" and "delete". The syntax for these commands
+    matches the API documentation exactly.
+    Perform Commands use the "perform" method, and are also listed in the API documentation.
+    These include commands such as "listHttpConnectionsPerSecondTests", "listTests" and "startTest".
+    Perform Commands are built from an OpenAPI.yaml file downloaded from the controller during instantiation.
+    These commands can be turned off, saving some start-up time. All functionality can still be accessed
+    via the HTTP verb methods.
+
+    Command Examples:
+        1. List all tests:
+            # HTTP Verb Method
+            cf.get("/tests")
+
+            # Perform Method
+            cf.perform("listTests")
+
+        2. List the tests named "Matt Test":        
+            # HTTP Verb Method
+            cf.get("/tests?filter[name]=Matt Test")
+
+            # Perform Method
+            cf.perform("listTests", filters={"name": "Matt Test"})
+
+        3. Change the duration of the LoadSpec for an EMix test:
+            # HTTP Verb Method #1
+            cf.put("/tests/emix/" + testid, config={'loadSpecification': {'duration': 500}})           
+            # HTTP Verb Method #2
+            cf.put("/tests/emix/" + testid, {"config": {'loadSpecification': {'duration': 600}}})
+
+            # Perform Method #1
+            cf.perform("updateEmixTest", testId=testid, config={'loadSpecification': {'duration': 700}})
+            # Perform Method #2
+            cf.perform("updateEmixTest", {"config": {'loadSpecification': {'duration': 800}}}, testId=testid)
+
+        4. Get all Test Run Results:
+            # HTTP Verb Method
+            cf.get("/test_runs/" + testrun["id"] + "/results/")
+
+            # Perform Method (Notice that we need to specify the "command_type" argument)
+            cf.perform("listTestRuns", command_type="Test Runs")
+
+        5. Get a Test Run Result:
+            # HTTP Verb Method
+            cf.get("/test_runs/" + testrun["id"] + "/results/" + testrunresults["id"])  
+
+            # Perform Method
+            cf.perform("getTestRunResult", testRunId=testrun["id"], testRunResultsId=testrunresults["id"])
+
+    Modification History:
+    1.0.0 : 07/10/2020 - Matthew Jefferson
+            -The initial code.
+
+    :copyright: (c) 2020 by Matthew Jefferson.
+"""
+
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import requests
+
+import os
+import sys
 import json
 import re
 import logging
-import os
-import sys
 import datetime
+
+# Required for processing error messages from the ReST API.
 import ast
-import inspect
+
+#import inspect
 import functools
+
+# Copy is require for the deepcopy function.
+import copy
+
+# pickle and yaml are required for processing the OpenAPI yaml file.
 import pickle
 import yaml
 
-import pprint
-pp = pprint.PrettyPrinter(indent=2)
-
+#==============================================================================
 def logging_decorator(func):
+    """This decorator is used to populate the logs with the Python client commands as they are executed.
+    """
     @functools.wraps(func)
     def wrapper_decorator(*args, **kwargs):
         logging.debug("ENTER=" + str(func) + " args=" + str(args) + " kwargs=" + str(kwargs))        
@@ -27,6 +107,40 @@ def logging_decorator(func):
         return value
     return wrapper_decorator 
 
+# Copyright Ferry Boender, released under the MIT license.
+def deepupdate(target, src):
+    """Deep update target dict with src
+    For each k,v in src: if k doesn't exist in target, it is deep copied from
+    src to target. Otherwise, if v is a list, target[k] is extended with
+    src[k]. If v is a set, target[k] is updated with v, If v is a dict,
+    recursively deep-update it.
+
+    Examples:
+    >>> t = {'name': 'Ferry', 'hobbies': ['programming', 'sci-fi']}
+    >>> deepupdate(t, {'hobbies': ['gaming']})
+    >>> print t
+    {'name': 'Ferry', 'hobbies': ['programming', 'sci-fi', 'gaming']}
+    """
+    for k, v in src.items():
+        if type(v) == list:
+            if not k in target:
+                target[k] = copy.deepcopy(v)
+            else:
+                target[k].extend(v)
+        elif type(v) == dict:
+            if not k in target:
+                target[k] = copy.deepcopy(v)
+            else:
+                deepupdate(target[k], v)
+        elif type(v) == set:
+            if not k in target:
+                target[k] = v.copy()
+            else:
+                target[k].update(v.copy())
+        else:
+            target[k] = copy.copy(v)
+
+#==============================================================================
 class CyberFlood:    
     def __init__(self, username, password, controller_address, perform_commands=True, log_level="INFO", log_path=None):
 
@@ -84,10 +198,7 @@ class CyberFlood:
         # The logger is now ready.        
         logging.info("Executing __init__: " + str(arguments))
 
-        # logging.basicConfig()
-        # logging.getLogger().setLevel(logging.DEBUG)
         requests_log = logging.getLogger("requests.packages.urllib3")
-        # requests_log.setLevel(logging.DEBUG)
         requests_log.propagate = True
 
         # Authenticate. This will allow all subsequent calls to use the token.
@@ -96,7 +207,6 @@ class CyberFlood:
         if response.status_code == 201:
             self.__bearerToken = json.loads(response.text)['token']
             self.__session.headers.update(Authorization='Bearer ' + self.__bearerToken)
-            #self.__isLogged = True
 
         if self.perform_commands:
             # Perform Commands are enabled. We need to download the OpenAPI.yaml file and 
@@ -113,37 +223,45 @@ class CyberFlood:
         return    
 
     @logging_decorator
-    def post(self, url, payload=None):
-        result = self.exec("post", url, payload)
+    def post(self, url, *args, **kwargs):
+        result = self.exec("post", url, *args, **kwargs)
         return result
 
     @logging_decorator
-    def delete(self, url, payload=None):
-        result = self.exec("delete", url, payload)
+    def delete(self, url, *args, **kwargs):
+        result = self.exec("delete", url, *args, **kwargs)
         return 
 
     @logging_decorator
-    def put(self, url, payload=None):              
-        result = self.exec("put", url, payload)
+    def put(self, url, *args, **kwargs):              
+        result = self.exec("put", url, *args, **kwargs)
         return result
 
     @logging_decorator
-    def get(self, url, payload=None):
-        result = self.exec("get", url, payload)
+    def get(self, url, *args, **kwargs):
+        result = self.exec("get", url, *args, **kwargs)
         return result
 
     @logging_decorator
-    def perform(self, command_name, command_type=None, payload=None, **kwargs):        
-
+    def perform(self, command_name, *args, command_type=None, **kwargs):         
+        """This method is used to execute "Perform Commands". These are the commands that are listed
+        in the CyberFlood API documentation. This method uses the CfCommand objects.
+        e.g.
+            getEmixTest
+            listTests
+            listTestRunResults
+        """       
         if not self.perform_commands:
             raise Exception("Perform Commands are not enabled. Use the perform_commands=True argument when initializing the CyberFlood client.")        
 
+        # Determine which command is being invoked.
         if command_name not in self.commands.keys():
             raise Exception("The command '" + command_name + "' is not valid.")        
 
         command_list = list(self.commands[command_name].keys())
 
         if len(command_list) > 1:
+            # There is more than one command with this command_name. The user must specify the command_type.
             if not command_type:
                 raise Exception("You must specify the type for this command.")                    
         else:            
@@ -151,30 +269,41 @@ class CyberFlood:
 
         command = self.commands[command_name][command_type]
 
-        result = command.perform(payload, **kwargs)            
+        result = command.perform(*args, **kwargs)            
 
-        return result
+        return result    
 
-    #def exec(self, httpverb, url, *args):        
-    def exec(self, httpverb, url, payload=None, filters=None, *args):        
+    def exec(self, httpverb, url, *args, filters=None, **kwargs):  
+        """Send the specified HTTP request to the CyberFlood ReST API.    
+        The filter argument is special. It is a dictionary of filters that must be added to the URL.  
+        """
 
+        # Construct the complete URL.
         url = self.controller_address + url
+        url += self._add_filters(filters)
 
-        # Extract any filters and add them to the URL.
-        url, args = self._get_filters(url, args)
-
+        # Construct the json_payload, which can be a combination of args and kwargs.
         payload = {}
+        json_payload = {}        
+
         if args:
-            payload = json.dumps(args[0])
+            # Only one positional arg is supported, and it must be a dictionary.
+            payload = args[0]
+
+        # Merge the kwargs into the payload dictionary.
+        deepupdate(payload, kwargs)
+
+        if len(list(payload.keys())) > 0:
+            json_payload = json.dumps(payload)
 
         httpverb = httpverb.lower()
 
         if httpverb == "get":
             response = self.__session.get(url)
         elif httpverb == "post":            
-            response = self.__session.post(url, data=payload, headers={'Content-Type': 'application/json'}, verify=False)
+            response = self.__session.post(url, data=json_payload, headers={'Content-Type': 'application/json'}, verify=False)
         elif httpverb == "put":
-            response = self.__session.put(url, data=payload, headers={'Content-Type': 'application/json'}, verify=False)            
+            response = self.__session.put(url, data=json_payload, headers={'Content-Type': 'application/json'}, verify=False)            
         elif httpverb == "delete":
             response = self.__session.delete(url)
         else:
@@ -185,18 +314,20 @@ class CyberFlood:
         
         return_value = None
 
+        # Process the response.
         content_disposition = response.headers.get("content-disposition")
 
         if response.headers.get("content-type") == "application/json":
             return_value = response.json()
         elif content_disposition and re.match("attachment", content_disposition, flags=re.I):
-            # The content-disposition will look something like this:
-            # attachment; filename="event.log"
+            # The response contained an attachment.
+            # The content-disposition will look something like this: attachment; filename="event.log"
             match = re.search("filename=\"(.+)\"", content_disposition, flags=re.I)            
             filename = match.group(1)
             return_value = self._save_file(response, filename)
         elif response.headers.get("content-type") == "application/octet-stream":
-            # This is likely a file that needs to be downloaded.
+            # This is probably a file as well. The last part of the URL should be the filename.
+            # e.g. "https://1.1.1.1/api/v2/documentation/openapi.yaml"
             if url.find('/'):
                 # Extract the filename from the URL.
                 filename = url.rsplit('/', 1)[1]
@@ -205,13 +336,15 @@ class CyberFlood:
             
             return_value = self._save_file(response, filename)
         else:
+            # Whoops...looks like we got a response that wasn't anticipated.
             logging.debug(str(response.headers.get))
             raise Exception("ERROR: Unknown response type (" + response.headers.get("content-type") + ").")
 
         return return_value
 
     def _save_file(self, response, filename, directory=None):        
-        # Save the attached file to the current directory (or possibly a subdirectory).
+        """ Save a file attachment from a response to the current directory (or possibly a subdirectory).
+        """
 
         if directory:
             filename = os.path.join(directory, filename)
@@ -234,26 +367,31 @@ class CyberFlood:
 
         return filename     
 
-    def _get_filters(self, url, args):
-        newurl = url
+    def _add_filters(self, filters):      
+        """Convert any filters, specified as a dictionary by the user, into a string for a URL.
+        """  
+        filtersurl = ""
+        if filters:            
+            for key in filters.keys():
+                if filtersurl != "":
+                    filtersurl += "&"
 
-        global pp
-        pp.pprint(args)
+                filtersurl += "filter[" + str(key) + "]=" + str(filters[key])
+            filtersurl = "?" + filtersurl
 
-        newargs = None
-
-        return newurl, newargs
+        return filtersurl
 
 
     def _process_error(self, response):
+        """Handle error responses from the CyberFlood ReST API.
+        """
         if response.text != "":
-
             # The error details should be a dictionary.
-            errordetails = ast.literal_eval(response.text)
+            # e.g.
             # {"type":"validation",
             #  "message":"Validation failed, config subnets client vlans id must be an integer",
             #  "errors":{"config":{"subnets":{"client":{"1":{"vlans":{"0":{"id":["must be an integer","must be greater than or equal to 0","must be less than or equal to 4094"]}}}}}}}}
-
+            errordetails = ast.literal_eval(response.text)
             errmsg = errordetails.get("message", "No message")        
 
             additionaldetails = errordetails.get("errors", None)
@@ -266,8 +404,8 @@ class CyberFlood:
         raise Exception(errmsg)      
 
     def _convert_yaml_to_dict(self, inputfilename):        
-
-        # Open and read the YAML input file.
+        """Open and convert the OpenAPI.yaml file to a Python dictionary.
+        """
         yamldict = {}
 
         try:
@@ -284,7 +422,9 @@ class CyberFlood:
 
         return yamldict
 
-    def _generate_classes(self):        
+    def _generate_classes(self):
+        """The method instantiates the perform commands, based on the OpenAPI.yaml file from the controller.
+        """        
         self.commands = {}
 
         for path in self.api_spec["paths"].keys():
@@ -299,8 +439,17 @@ class CyberFlood:
 
         return         
 
+#==============================================================================
 class CfCommand:    
-    """This class is used to store
+    """This class defines each of the perform commands for CyberFlood.
+    A perform command is an command that is used with the perform method.
+    e.g.
+        getEmixTest
+        listTests
+        listTestRunResults
+
+    These commands are grouped by type (tag), such as "HTTP Throughput Tests", "Devices" and "Subnets".
+    Some commands are found in more than one group, requiring the type also be specified with the command.
     """
     def __init__(self, cyberfloodobject, path, httpverb, definition):
         self.cf = cyberfloodobject
@@ -332,23 +481,24 @@ class CfCommand:
         return
 
     @logging_decorator
-    def perform(self, payload=None, filters=None, **kwargs):
-        path = self.resolve_path(**kwargs)
+    def perform(self, *args, **kwargs):
+        # Generate the resolvedpath by replacing the path argument names with 
+        # the user-specified values for each argument.
+        # All arguments found in the path are required.
+        # e.g. path = '/tests/{testId}/results/{testResultId}'
+        #     returns = '/tests/lkj43lkjfi34flklksflkji43jlfrl2/results/b5fb4a9e322c4333805aa9e13c433f85'                
+        resolvedpath = self.path
+        for arg in re.findall("{.+?}", self.path):
+            # remove the brackets for the arg to find the dictionary key.
+            key = re.sub("[{}]", "", arg)
+            if key not in kwargs.keys():
+                raise Exception("The argument '" + key + "' is required for the command " + self.name + " (" + self.tag + ").")        
+        
+            resolvedpath = re.sub(arg, kwargs[key], resolvedpath)  
+            
+            #Remove this key, so that it doesn't get added to the HTTP payload.      
+            kwargs.pop(key)            
 
-        result = self.cf.exec(self.httpverb, path, payload, filters)
+        result = self.cf.exec(self.httpverb, resolvedpath, *args, **kwargs)
 
         return result
-
-    def resolve_path(self, **kwargs):
-        """Generate the command path by replacing the parameter "placeholders" names with 
-        the user-specified values for each parameter.
-        e.g. path = '/tests/{testId}/results/{testResultId}'
-             returns = '/tests/lkj43lkjfi34flklksflkji43jlfrl2/results/b5fb4a9e322c4333805aa9e13c433f85'
-        """
-
-        path = self.path
-        for key in kwargs.keys():
-            path = re.sub("{" + key + "}", kwargs[key], path)        
-
-        return path
-
